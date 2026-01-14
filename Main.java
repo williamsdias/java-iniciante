177da72e71e8d70d2d6fd4900727b99dc1167d76
williamsdias <williamsouzadias.pessoal@gmail.com>
2026-01-13 23:03:02 -0300

    Adiciona primeiro programa em Java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+        System.out.println("Meu nome é William Souza Dias");
+        System.out.println("Iniciando meus estudos em Java");
+    }
+}
